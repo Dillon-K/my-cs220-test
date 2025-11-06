90557e05231cad9dfaffd815291ad46487fe966c
Dillon K <dillonkung@gmail.com>
2025-11-06 13:05:32 -0800

    string stuff

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
 int main()
 {
     cout << "hello world" << endl;
     cout << "This is fun!" << endl;
+
+    string s = "abc";
+    cout << s << endl;
 }
